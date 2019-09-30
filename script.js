f840a3e5f4a2a77f6e9282f716f5a1f7db2d257c
mianak78 <rosebourn@gmail.com>
2019-09-29 22:55:42 -0600

    Sunday Night

--- script.js
+++ script.js
@@ -8,47 +8,48 @@ var howMany = prompt("How many characters would you like in the password? (Must 
 var specCharQues = confirm("Would you like the password to contain special characters?");
 var numQues = confirm("Would you like the password to have numbers?");
 var upperQues = confirm("Would you like the password to have uppercase letters?");
+var lowQues = confirm("Would you like the password to have lowercase letters?");
 
 alert(howMany);
 alert(specCharQues);
 alert(numQues);
 alert(upperQues);
+alert(lowQues);
 
-var randomCap = capLet[Math.floor(Math.random()*capLet.length)];
+var randomCap = capLet[Math.floor(Math.random() * capLet.length)];
 console.log(randomCap);
-var randomLow = lowLet[Math.floor(Math.random()*lowLet.length)];
+var randomLow = lowLet[Math.floor(Math.random() * lowLet.length)];
 console.log(randomLow);
-var randomNum = num[Math.floor(Math.random()*num.length)];
+var randomNum = num[Math.floor(Math.random() * num.length)];
 console.log(randomNum);
-var randomChar = specialChar[Math.floor(Math.random()*specialChar.length)];
+var randomChar = specialChar[Math.floor(Math.random() * specialChar.length)];
 console.log(randomChar);
 
-//var password = randomLow.concat(randomCap, randomNum, randomChar);
-//console.log(password);
-
-//let i = howMany;
 var password = "";
 
+// Trying to get a password with capital and lowercase letters
 function passOne () {
-   for (i = 0; i < howMany; i++) {
-    if (password.length >= howMany) {
+if (upperQues && lowQues) {
+    for (i = 0; i < howMany; i++) {
+       if (password.length >= howMany) {
         break;
-    }
-    if (upperQues) {
-     password + randomCap + randomLow;
     } 
+    password + randomCap + randomLow;
+    }
+    
 }
 }
-//passOne (randomCap, randomLow)
+passOne(randomCap + randomLow);
+console.log(password);
 
-var password = randomLow.concat(randomCap);
-    console.log(password);
+// After figuring out a workig function, can hopefully plug in the other combos to make passwords.
+// Link generate button to HTML.
+// Get password to print to text box.
+// Write code for copy to clipboard.
 
 
-//if (confirm(specCharQues) && confirm(numQues) && confirm(upperQues)) {
-    //for (i = 0; i >= 2 && i <= 32; i++) {
-        //if ()
-        //var password = randomLow.concat(randomCap, randomNum, randomChar);
-        //console.log(password[i]);
-    //} 
-//}
+// This is just random code that I used at different times during the project but wasn't sure I wanted to get rid of.
+//var password = randomLow.concat(randomCap, randomNum, randomChar);
+//console.log(password); 
+//var password = randomLow.concat(randomCap);
+
