cab72cee671f0e3f886e14d8b63ce1d5f1afc8e2
mianak78 <rosebourn@gmail.com>
2019-10-02 10:04:47 -0600

    Wed commit

--- script.js
+++ script.js
@@ -1,46 +1,80 @@
 
-var capLet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowLet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "?", "#", "$", "%", "^", "&", "*", "_", "+", "/"];
+//var capLet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//var lowLet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//var specialChar = ["!", "?", "#", "$", "%", "^", "&", "*", "_", "+", "/"];
 
 var howMany = prompt("How many characters would you like in the password? (Must be at least 8 characters but no longer than 128.)")
 var specCharQues = confirm("Would you like the password to contain special characters?");
 var numQues = confirm("Would you like the password to have numbers?");
 var upperQues = confirm("Would you like the password to have uppercase letters?");
-var lowQues = confirm("Would you like the password to have lowercase letters?");
+//var lowQues = confirm("Would you like the password to have lowercase letters?");
 
 alert(howMany);
 alert(specCharQues);
 alert(numQues);
 alert(upperQues);
-alert(lowQues);
+//alert(lowQues);
 
-var randomCap = capLet[Math.floor(Math.random() * capLet.length)];
-console.log(randomCap);
-var randomLow = lowLet[Math.floor(Math.random() * lowLet.length)];
-console.log(randomLow);
-var randomNum = num[Math.floor(Math.random() * num.length)];
-console.log(randomNum);
-var randomChar = specialChar[Math.floor(Math.random() * specialChar.length)];
-console.log(randomChar);
+//var randomCap = capLet[Math.floor(Math.random() * capLet.length)];
+//console.log(randomCap);
+//var randomLow = lowLet[Math.floor(Math.random() * lowLet.length)];
+//console.log(randomLow);
+//var randomNum = num[Math.floor(Math.random() * num.length)];
+//console.log(randomNum);
+//var randomChar = specialChar[Math.floor(Math.random() * specialChar.length)];
 
-var password = "";
 
-// Trying to get a password with capital and lowercase letters
-function passOne () {
-if (upperQues && lowQues) {
-    for (i = 0; i < howMany; i++) {
-       if (password.length >= howMany) {
-        break;
-    } 
-    password + randomCap + randomLow;
+//var password = "";
+
+var capLet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar = ["!", "?", "#", "$", "%", "^", "&", "*", "_", "+", "/", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var capSpec = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "?", "#", "$", "%", "^", "&", "*", "_", "+", "/"];
+var capNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+if (upperQues) {
+    for (var i = 0; i < howMany; i++) {
+        var password = Math.floor(Math.random() * capLet.length);
+        console.log(password);
     }
     
 }
+
+if (specCharQues) {
+    for (var i = 0; i < howMany; i++) {
+        var password = Math.floor(Math.random() * specialChar.length);
+        console.log(password);
+    }
 }
-passOne(randomCap + randomLow);
-console.log(password);
+
+if (numQues) {
+    for (var i = 0; i < howMany; i++) {
+        var password = Math.floor(Math.random() * num.length);
+        console.log(password);
+    }
+}
+
+if (upperQues && specCharQues) {
+    for (var i = 0; i < howMany; i++) {
+        var password = Math.floor(Math.random() * capSpec.length);
+        console.log(password);
+    }
+}
+
+if (upperQues && numQues) {
+    for (var i = 0; i < howMany; i++) {
+        var password = Math.floor(Math.random() * capSpec.length);
+        console.log(password);
+    }
+}
+
+
+
+
+
+
 
 // After figuring out a workig function, can hopefully plug in the other combos to make passwords.
 // Link generate button to HTML.
