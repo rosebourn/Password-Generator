f826e81c25c62362f2be042aaa0365eb3663bc7b
mianak78 <rosebourn@gmail.com>
2019-10-02 22:21:00 -0600

    final for now

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var lowLet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "
 var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "?", "#", "$", "%", "^", "&", "*", "_", "+", "/"];
 
-var howMany = prompt("How many characters would you like in the password? (Must be at least 8 characters but no longer than 128.)")
+var howMany = prompt("How many characters would you like in the password? (Must be at least 8 characters but no longer than 128.)");
 var specCharQues = confirm("Would you like the password to contain special characters?");
 var numQues = confirm("Would you like the password to have numbers?");
 var upperQues = confirm("Would you like the password to have uppercase letters?");
@@ -63,4 +63,24 @@ function copyPassword() {
     alert("Password copied to clipboard");
 }
 
+/* var howMany = prompt("How many characters would you like in the password? (Must be at least 8 characters but no longer than 128.)")
+var morePlz = (howMany < 8);
+var lessPlz = (howMany > 128);
+var justRight = (8 < howMany < 128);
+if (morePlz) {
+    alert("Must have at least 8 characters!");
+}
+else {
+    generate();
+}
 
+if (lessPlz) {
+    alert("Must be less than 128 characters!");
+}
+else {
+    generate();
+}
+
+if (justRight) {
+    generate();
+} */
